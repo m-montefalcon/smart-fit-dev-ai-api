6c432aa2f2b9f2590d9e871d41285f329259eb94
m-monefalcon <mmontefalconschlprps@gmail.com>
2025-04-18 18:58:38 +0000

    added users migration

---
+++ src/migrations/1744992334715-CreateUsersTable.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersTable1744992334715 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'full_name',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+            length: '100',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+            length: '100',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
